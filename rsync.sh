9f431adb7640b3e4413e7ded40c8f9876daf17a8
zhangxu9 <zhangxu9@leju.com>
2017-02-14 15:58:03 +0800

    update

--- rsync.sh
+++ rsync.sh
@@ -8,43 +8,61 @@ online_dir=$3
 commit_msg=$4
 iNow=0
 
-# # 文件目录转换为数组
-# for s in ${files[@]}
-# do
-# 	files[$iNow]=$s
-# 	iNow=`expr $iNow + 1`
-# done
-# IFS="$OLD_IFS"
-echo '待移动文件目录：'
-echo ${files[*]}
+echo ""
+echo "***************待移动文件路径："
+# 文件目录转换为数组
+for s in ${files[@]}
+do
+	echo $s
+done
+IFS="$OLD_IFS"
+
 # 检测测试环境当前目录是否存在
 if [ ! -d $test_dir ]
 	then
-	echo "测试环境的文件目录 ( $test_dir ) 未找到"
+	echo "[ERROR] 测试环境的文件目录 ( $test_dir ) 未找到"
 	exit 1
 fi
+
 # 检测生成环境当前目录是否存在
 if [ ! -d $online_dir ]
 	then
-	echo "生产环境的文件目录 ( $online_dir ) 未找到"
+	echo "[ERROR] 生产环境的文件目录 ( $online_dir ) 未找到"
 	exit 1
 fi
 
 # 执行覆盖文件操作
-echo "复制文件到正式线中..."
+echo ""
+echo "***************移动文件到正式线："
 
+# 更新正式线代码
+echo "***************正式线版本号："
 svn up $online_dir
 
+# 移动文件到正式线
 cd $test_dir
-for path in $files
+for path in ${files[@]}
 do
 	echo "移动 $path 到 $online_dir"
 	rsync -ravu --delete $path $online_dir
 done
 
-echo ""
-
+# svn对比提交代码
 cd $online_dir
-echo "改动文件"
+echo ""
+echo "***************改动文件状态："
 echo `svn status`
+
+# 部署线上 svn
+echo ""
+echo "***************提交正式线svn："
+svn status | grep ^! | xargs | sed 's/\!//g'| xargs svn rm
+svn status | grep ^? | xargs | sed 's/\?//g'| xargs svn add
+# svn ci -m $commit_msg
+
+echo ""
+echo "***************提交完成"
+exit 0
+
+
 
